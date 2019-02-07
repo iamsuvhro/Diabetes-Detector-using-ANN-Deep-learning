1b08ec4c49a50132523272e1e4e6d692219c3928
Suvhradip ghosh <44000819+suvhradipghosh07@users.noreply.github.com>
2019-02-07 12:34:14 +0530

    Add files via upload

---
+++ diabetes.py
@@ -0,0 +1,48 @@
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('diabetes.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 8].values
+
+# Encoding categorical data
+
+# Part 2 - Now let's make the ANN!
+
+# Importing the Keras libraries and packages
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+# Initialising the ANN
+classifier = Sequential()
+
+# Adding the input layer and the first hidden layer
+classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu', input_dim = 8))
+
+# Adding the second hidden layer
+classifier.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'relu'))
+
+
+# Adding the output layer
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+
+# Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+# Fitting the ANN to the Training set
+classifier.fit(X_train, y_train, batch_size = 4, epochs = 100)
+
+# Part 3 - Making predictions and evaluating the model
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+y_pred = (y_pred > 0.5)
+
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
